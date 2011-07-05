1131089fb4d82c6483c851a0e10d05493bf54fcb
zhangsf <zhangsf@192.168.1.98>
2011-07-05 11:50:57 +0800

    delete files

--- pn/db/__init__.py
+++
@@ -1,7 +0,0 @@
-class DatabaseError(Exception):
-	pass
-
-def init(dbModule):
-	mod = __import__(dbModule, {}, {}, [''])
-	globals()['DatabaseError'] = getattr(mod, 'DatabaseError')
-	
--- pn/web/__init__.py
+++
@@ -1,5 +0,0 @@
-"""This module is from Twisted subproject web2
-
-becuase it can not be used with Twisted 2.5, so we just make a copy of it
-and remove what we don't need.
-"""
